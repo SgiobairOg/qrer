d314cc204ba0b01425aa96a3245be8dc8e18e52c
Jason Wilson <jason@wilsons.io>
2019-05-01 23:22:44 +0200

    fix(Refactor): Clean Up Code

--- packages/chrome/src/js/init.js
+++ packages/chrome/src/js/init.js
@@ -7,11 +7,6 @@ function init() {
 
   browser.tabs.query({ currentWindow: true, active: true }, function(tabs) {
     var urlString = tabs[0].url;
-    var options = {
-      width: 256,
-      height: 256,
-      correctLevel: QRCode.CorrectLevel.M
-    };
     var code = new QRCode("qrcode", urlString);
   });
 }
--- packages/firefox/src/js/init.js
+++ packages/firefox/src/js/init.js
@@ -5,7 +5,7 @@ window.onload = function() {
 function init() {
   if (!browser) var browser = chrome;
   browser.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-    console.log(tabs[0].url);
-    new QRCode(document.getElementById("qrcode"), tabs[0].url);
+    var urlString = tabs[0].url;
+    var code = new QRCode("qrcode", urlString);
   });
 }
